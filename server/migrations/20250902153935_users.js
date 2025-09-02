4c7d17542ec9badf86983b7547881d807eee445c
yepitsaname <fernando.g.berry@gmail.com>
2025-09-02 12:08:00 -0400

    adds a migration for users

---
+++ server/migrations/20250902153935_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('userID');
+    table.string('first_name', 40); // Based off of UK Gov Data Standards Catalog of 35 characters
+    table.string('last_name', 40); // Based off of UK Gov Data Standards Catalog of 35 characters
+    table.string('username', 16); // Arbitrary limit
+    table.string('password',16);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
