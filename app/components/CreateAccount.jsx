22b619bb08a1fd4c258b6f1979bf92eb2a1573f2
yepitsaname <fernando.g.berry@gmail.com>
2025-09-02 15:05:47 -0400

    passes all display tests for account creation form

--- app/components/CreateAccount.jsx
+++ app/components/CreateAccount.jsx
@@ -1,4 +1,25 @@
 export default function CreateAccount(){
 
-  return (<></>)
+  return (
+    <form>
+      <h2>Account Creation</h2>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" name="username" placeholder="username" maxLength={16} />
+
+      <label htmlFor="first-name">First Name</label>
+      <input type="text" id="first-name" name="first name" placeholder="First name..." maxLength={40} />
+
+      <label htmlFor="last-name">Last Name</label>
+      <input type="text" id="last-name" name="last name" placeholder="Last name..." maxLength={40} />
+
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="****************" maxLength={16} />
+
+      <label htmlFor="confirm-password">Confirm password</label>
+      <input type="password" id="confirm-password" name="confirm password" placeholder="****************" maxLength={16} />
+
+      <button type="submit">Create Account</button>
+      <button type="button">Cancel</button>
+    </form>
+  )
 }
--- app/tests/CreateAccount.test.jsx
+++ app/tests/CreateAccount.test.jsx
@@ -7,26 +7,30 @@ describe("Create Account Functionality", ()=>{
       render(<CreateAccount />)
     })
     it("should have a title", ()=>{
-      expect(screen.getByText('Account Creation')).toBeIntheDocument();
+      expect(screen.getByText('Account Creation')).toBeInTheDocument();
     })
     it("should have a username field", ()=>{
-      expect(screen.getByText('Username')).toBeIntheDocument();
+      expect(screen.getByText('Username')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('username')).toBeInTheDocument();
     })
     it("should have a first name field", ()=>{
-      expect(screen.getByText('First Name')).toBeIntheDocument();
+      expect(screen.getByText('First Name')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('First name...')).toBeInTheDocument();
     })
     it("should have a last name field", ()=>{
-      expect(screen.getByText('Last Name')).toBeIntheDocument();
+      expect(screen.getByText('Last Name')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Last name...')).toBeInTheDocument();
     })
     it("should have two password fields", ()=>{
-      expect(screen.getByText('Password')).toBeIntheDocument();
-      expect(screen.getByText('Confirm password')).toBeIntheDocument();
+      expect(screen.getByText('Password')).toBeInTheDocument();
+      expect(screen.getByText('Confirm password')).toBeInTheDocument();
+      expect(screen.getAllByPlaceholderText('****************').length).toBe(2);
     })
     it("should have a create account button", ()=>{
-      expect(screen.getByText('Create Account')).toBeIntheDocument();
+      expect(screen.getByText('Create Account')).toBeInTheDocument();
     })
     it("should have a cancel button", ()=>{
-      expect(screen.getByText('Cancel')).toBeIntheDocument();
+      expect(screen.getByText('Cancel')).toBeInTheDocument();
     })
   })
 })
