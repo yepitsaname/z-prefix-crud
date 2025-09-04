587c91511e7a479c503c2fcea36b9ecdf7863cce
yepitsaname <fernando.g.berry@gmail.com>
2025-09-04 12:08:05 -0400

    adds styling to the top nav and links

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -21,12 +21,7 @@ export default function Home(){
 
   return (
     <div>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
+      <h3>All Items</h3>
       {items.map(item=><Item key={item.item_id} item={item} noAuth={true} />)}
     </div>
   )
