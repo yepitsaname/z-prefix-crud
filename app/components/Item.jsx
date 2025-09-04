37a4facaa0ff09103d5b32368580f6689f4372af
yepitsaname <fernando.g.berry@gmail.com>
2025-09-03 21:04:42 -0400

    adds a function to trim item descriptions down to 100 characters for display

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -2,6 +2,18 @@ import '../css/item.css';
 
 export default function Item({item}){
 /**
+ * @param {string} description
+ */
+const trimDescription = (description) => {
+  if(description.length <= 100){ return description }
+  let stringArray = ['...'];
+  for( let i = 97; i >= 0; i-- ){
+    stringArray.unshift(description.charAt(i));
+  };
+  return stringArray.join("")
+}
+
+/**
  * @typedef item
  * @property {number} item_id
  * @property {number} user_id
@@ -13,7 +25,7 @@ export default function Item({item}){
     <div className='item'>
       <p className='id'>{item.item_id}</p>
       <p className='name'>{item.name}</p>
-      <p className="description">{item.description}</p>
+      <p className="description">{trimDescription(item.description)}</p>
       <p className='quantity'>{item.quantity}</p>
     </div>
   )
